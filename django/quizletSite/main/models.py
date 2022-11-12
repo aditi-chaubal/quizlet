c8da9089cd2222eaf75732275f37cf5181f3bffa
Aditi Chaubal <aditiachaubal@gmail.com>
2022-11-12 14:56:24 -0600

    added admin dashboard, Flashcard class

--- django/quizletSite/main/models.py
+++ django/quizletSite/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Flashcard(models.Model):
+    name = models.CharField(max_length = 300)
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    flashcard = models.ForeignKey(Flashcard, on_delete = models.CASCADE)
+    text = models.CharField(max_length = 300)
+
+    def __str__ (self):
+        return self.text
